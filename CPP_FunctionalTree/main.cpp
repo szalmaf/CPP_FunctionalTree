8db553a2ce4d9478b18fa1c5c222827776af5c15
Ferenc Szalma <szalmaf@gmail.com>
2017-01-20 17:37:39 -0500

    Initial Commit

---
+++ CPP_FunctionalTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP_FunctionalTree
+//
+//  Created by Ferenc Szalma on 1/20/17.
+//  Copyright © 2017 Ferenc Szalma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
