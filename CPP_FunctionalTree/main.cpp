5736e21cd6076517b0454de4e5e156c6265efa14
Ferenc Szalma <szalmaf@gmail.com>
2017-01-20 17:45:20 -0500

    Initial checkin. Add web source of project

--- CPP_FunctionalTree/main.cpp
+++ CPP_FunctionalTree/main.cpp
@@ -5,6 +5,7 @@
 //  Created by Ferenc Szalma on 1/20/17.
 //  Copyright © 2017 Ferenc Szalma. All rights reserved.
 //
+// https://bartoszmilewski.com/2013/11/25/functional-data-structures-in-c-trees/
 
 #include <iostream>
 
